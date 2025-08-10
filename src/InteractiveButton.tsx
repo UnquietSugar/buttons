e062954afac4c93bc0e384330f98e8c94f3a3b00
kiril10speed <kiril@10speed.lt>
2025-08-10 11:16:08 +0300

    Add button component

---
+++ src/InteractiveButton.tsx
@@ -0,0 +1,23 @@
+import "./index.css";
+import { useEffect, useState } from "react";
+
+export const InteractiveButton = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    if (!isActive) return;
+
+    const timeout = setTimeout(() => {
+      setIsActive(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, [isActive]);
+
+  return (
+    <button
+      className={`button-container ${isActive ? "active" : ""}`}
+      onClick={() => setIsActive(true)}
+    />
+  );
+};
